5148c20038c7a7d198787b620e8b8d960972c87b
amar <amarbadiger45@gmail.com>
2024-08-07 23:12:55 +0530

    some changes

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ app.use("/api/v1/hero", require("./routes/contactFormRoute"));
 app.use("/api/v1/", require("./routes/PostRoute"));
 app.use("/api/v1/feeds", require("./routes/FeedRoute"));
 
-app.use(express.static(path.join(__dirname, "/dist")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
 
 // Port
