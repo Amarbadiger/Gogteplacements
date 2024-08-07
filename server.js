3b9a09b178de55097f42d8b85ad6d3196b55b653
amar <amarbadiger45@gmail.com>
2024-08-07 23:03:21 +0530

    something changes in server.js

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const morgan = require("morgan");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const path = require("path");
-const fs = require("fs");
 
 // dotenv config
 dotenv.config();
 const app = express();
 
+app.use(cors());
+
 // MongoDB connection
 connectDB();
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
@@ -25,36 +25,13 @@ app.use("/api/v1/hero", require("./routes/contactFormRoute"));
 app.use("/api/v1/", require("./routes/PostRoute"));
 app.use("/api/v1/feeds", require("./routes/FeedRoute"));
 
-// Serve static files from the 'dist' directory
-const distPath = path.join(__dirname, "dist");
-console.log("Serving static files from:", distPath);
+app.use(express.static(path.join(__dirname, "/dist")));
 
-// Verify if dist directory exists
-if (fs.existsSync(distPath)) {
-  console.log("Dist directory exists");
-} else {
-  console.error("Dist directory does not exist");
-}
-
-app.use(express.static(distPath));
-
-// Catchall handler: for any request that doesn't match above, send back index.html
 app.get("*", (req, res) => {
-  const indexPath = path.join(distPath, "index.html");
-  console.log("Request received for:", req.originalUrl);
-  console.log("Index file path:", indexPath);
-
-  if (fs.existsSync(indexPath)) {
-    res.sendFile(indexPath);
-  } else {
-    res.status(404).send("index.html not found");
-  }
+  res.sendFile(path.join(__dirname, "/dist", "index.html"));
 });
 
 // Port
-const port = process.env.PORT || 8080;
-
-// Start server
-app.listen(port, () => {
-  console.log(`server is running at ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log("Server is running at 8000");
 });
